752ca10f0cda94adaf63df8971cf05f6ee03603d
XT <xt@xorde.co>
2022-08-04 19:05:17 -0400

    Fixes #1 (hopefully)

--- src/p.ts
+++ src/p.ts
@@ -1,31 +1,42 @@
+// Thanks to https://stackoverflow.com/a/58436959/16323129 for a hint how to set specified depth
+
+type Prev = [never, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, ...0[]];
+
+type Join<K, P> = K extends string | number
+	? P extends string | number
+		? `${K}${'' extends P ? '' : '.'}${P}`
+		: never
+	: never;
+
+type Paths<T, D extends number = 10> = [D] extends [never]
+	? never
+	: T extends object
+	? {
+			[K in keyof T]-?: K extends string | number ? `${K}` | Join<K, Paths<T[K], Prev[D]>> : never;
+	  }[keyof T]
+	: never;
+
 /**
- * Picks selected properties from the object
+ * Picks selected properties from the object up to 3 levels deep
  * @param obj Original object
  * @param props Array of original object properties
  * @return obj Object with picked properties
  */
-type NestedKeyOf<ObjectType extends object> =
-	{
-		// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-		// @ts-ignore
-		/* prettier-ignore */ [Key in keyof ObjectType & (string | number)]: ObjectType[Key] extends object ? `${Key}` | `${Key}.${NestedKeyOf<ObjectType[Key]>}` : `${Key}`;
-	}[keyof ObjectType &
-		(string | number)];
-
-export const p = <T extends object>(obj: T, props: (NestedKeyOf<T> & (string | number))[]) => {
+export const p = <T extends object>(obj: T, props: Array<Paths<T, 3>>) => {
 	const accumulator = {};
+
 	props.forEach((prop: any) => {
 		const separator = (path: string) => path.indexOf('.');
 		const parent = (path: string, separator: number) => path.substring(0, separator);
 		const child = (path: string, separator: number) => path.substring(separator + 1);
 		const getNestedValue = (obj: any, path: string): any => {
 			const s = separator(path);
-			// console.debug('s:', s, 'path:', path);
+
 			return s == -1 ? obj[path] : getNestedValue(obj[path.substring(0, s)], path.substring(s + 1));
 		};
 		const assignNestedValue = (obj: any, path: string, value: any): any => {
 			const s = separator(path);
-			// console.debug('obj:', obj, 'path:', path, 'value:', value, 'parent:', parent(path, s));
+
 			if (s == -1) {
 				obj[path] = value;
 				return obj;
@@ -40,6 +51,6 @@ export const p = <T extends object>(obj: T, props: (NestedKeyOf<T> & (string | n
 			assignNestedValue(accumulator, prop, value);
 		}
 	});
-	// console.debug('p:', accumulator);
+
 	return accumulator;
 };
